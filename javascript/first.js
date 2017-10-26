9a50128afe0cded37d70adefd8ffb731d0e31880
AAKASH HANDA <AAKASH HANDA>
2017-10-26 20:06:45 +0530

    added javascript

---
+++ javascript/first.js
@@ -0,0 +1,110 @@
+string
+number
+boolean
+
+"jaya"
+"1234"
+"true"
+12
+12.025
+312315645515
+true/false
+
+str a ="jaya"
+int a =54545
+bool a =true
+//es5
+var a="jaya";
+var b= 41.32
+var c= true
+
+//es6
+let a="jaya";
+let b= 41.32
+let c= true
+
+/////////////////////////////
+var a = "javascript"
+undefined
+a
+"javascript"
+typeof(a)
+"string"
+var a = "javascript"
+undefined
+var a = 20
+undefined
+a
+20
+typeof(a)
+"number"
+var a = "20"
+undefined
+typeof(a)
+"string"
+var c = true
+undefined
+typeof(c)
+"boolean"
+
+
+
+1+1
+2
+5/2
+2.5
+9-3
+6
+8*2
+16
+var a =10
+undefined
+var b=20
+undefined
+a+b
+30
+5%2
+1
+4%2
+0
+
+var a = "jaya  "
+undefined
+var b ="javascript"
+undefined
+a+b
+"jaya  javascript"
+a-b
+NaN
+var c =10
+undefined
+a+c
+"jaya  10"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
+
+
+10>1
+true
+10<1
+false
+10==10
+true
+"10"==10
+true
+"10"===10
+false
+isNaN("jaya")
+true
+isNaN(10)
+false
+
+
+console.log("jaya")
+VM447:1 jaya
+undefined
+alert("jaya")
---
+++ javascript/second.js
@@ -0,0 +1,26 @@
+var a = 10;
+
+if(a%2==0){
+    console.log("number is even")
+}else if(a%3==0){
+    console.log("number is divisible by 3")
+}
+else{
+    console.log("number is odd")
+}
+
+var a = "jaya"
+if(a=="jaya"){
+    console.log("allow access")
+}else if(a=="aakash"){
+    console.log("dont allow access")
+}else{
+    console.log("i don't know")
+}
+
+var a = "jaya"
+if(isNaN(a)){
+    console.log("given is NaN")
+}else{
+    console.log("given is number")
+}
